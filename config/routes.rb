9e536aa06df90e29d928859344e2226da5850933
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-06-29 14:13:52 +0000

    Book&Userのアソシエーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'about' => 'homes#about'
   devise_for :users
+  resources :books, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
