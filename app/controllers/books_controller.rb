e92e31e217fed78986f5e25147c82346183c9720
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-03 12:09:18 +0000

    サクセスメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfly."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -35,7 +36,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      flash[:notice] = "You have updated book successfly."
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
