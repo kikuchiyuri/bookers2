23d2fa4a000aa09ad637c23b884a06a5b4f55cde
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-04 09:48:48 +0000

    Bootstrap,FontAwesomeの導入

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
 
-  #get '*path', controller: 'application', action: 'render_404'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
