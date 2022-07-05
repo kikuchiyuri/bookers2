349225856efed17d628ce939ab2c77fbaffb516d
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-07-05 12:54:30 +0000

    RSpecテスト導入、ユーザー一覧画面レイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :ensure_current_user, {only: [:edit, :update]}
+
   def index
     @users = User.all
     @book = Book.new
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "You have updated user successfly."
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       render :edit
@@ -30,6 +33,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def ensure_current_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
